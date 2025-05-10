f5afba7d5e4c42568eb43409d21641684717df29
TuncayHp <tunc270@gmail.com>
2025-05-11 00:39:33 +0300

    up

---
+++ FiComponents/FiDataGridHelper.cs
@@ -0,0 +1,21 @@
+﻿using OrakYazilimLib.DbGeneric;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace OrakUtilWpf.FiComponents
+{
+  public class FiDataGridHelper
+  {
+    public static DataGridTextColumn GenDataGridCol(FiCol fiCol)
+    {
+
+      DataGridTextColumn column = new DataGridTextColumn
+      {
+        Header = fiCol.ofcTxHeader,
+        Binding = new Binding($"[{fiCol.ofcTxFieldName}]")
+      };
+
+      return column;
+    }
+  }
+}
